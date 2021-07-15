e7fd3c02f9dcd43a1179df753181b4d3fb9bef5d
Hrushikesh <hrushi.s.shinde@gmail.com>
2021-07-15 17:56:00 +0530

    Massage change in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,5 +2,5 @@ c public class Sum{
 	public static void main(String[] a){
 		int num1=10,num2=20,sum;
 		sum=num1+num2;
-		System.out.println("sum of these numbers: "+sum);
+		System.out.println("sum of numbers is: "+sum);
 	}
